b99bf67c387d3e26d3641ede7a45f12454e554f6
Eugene Denkevich <e.denkevich@trainingdata.pro>
2024-03-02 22:44:24 +0300

    add telebot

--- bot/services.py
+++ bot/services.py
@@ -1,22 +1,24 @@
-from asgiref.sync import async_to_sync
-from aiogram import Dispatcher
-from aiogram import Bot
+from telebot import TeleBot
 import redis
 
 from bot.settings import BOT_TOKEN
 
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+bot = TeleBot(token=BOT_TOKEN)
 
 
 class BotService:
     """Сервис бота"""
     
-    def send_message(self, chat_id, message) -> None:
-        """Отправка ботом сообщения"""
-        connection = redis.Redis()
+    def send_message_redis(self, chat_id, message) -> None:
+        """Отправка ботом сообщения используя Redis"""
+        connection = redis.Redis()     
         connection.publish("chat_id", f"{str(chat_id)}:::{str(message)}")
         # TODO Сделать создание connection в другом месте.
+
+    def send_message(self, chat_id, message) -> None:
+        """Отправка ботом сообщения"""
+        bot.send_message(chat_id=chat_id, text=message)
+
 
 bot_service = BotService()
