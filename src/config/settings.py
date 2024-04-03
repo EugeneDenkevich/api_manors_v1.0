a87bd44bccbd908b177c2fac30a204ed1e445614
Eugene Denkevich <e.denkevich@trainingdata.pro>
2024-04-03 21:50:35 +0300

    fix BASE_DIR

--- src/config/settings.py
+++ src/config/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "src" / STATIC_URL
+STATIC_ROOT = BASE_DIR / "src" / "static"
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "src" / MEDIA_URL
+MEDIA_ROOT = BASE_DIR / "src" / "static"
 
 LOGGING = {
     'version': 1,
